3d6f8e6906e0c3be39b708910b8a792fbc6befb2
Wiphawanyaun <107971015+Wiphawanyaun@users.noreply.github.com>
2023-08-05 11:42:39 +0700

    Second Commit [Week6-August-05-2023]

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TouchableExample from './components/TouchableExample'
 import TouchablePractice from './components/TouchablePractice'
+import Example_useEffect from './components/Example_useEffect'
+import UseEffectFlatList from './components/UseEffectFlatList'
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/* <TouchableExample/> */}
-      <TouchablePractice/>
+      {/* <TouchablePractice/> */}
+      {/* <Example_useEffect/> */}
+      <UseEffectFlatList/>
     </View>
   )
 }
