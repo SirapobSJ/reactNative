077ef03ad20c3f94dfb7f516471262c4a6956e90
Wiphawanyaun <107971015+Wiphawanyaun@users.noreply.github.com>
2023-09-09 11:49:49 +0700

    9/9/2023

---
+++ App_PassingParams.js
@@ -0,0 +1,36 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreatePostScreen from "./screens/CreatePostScreen";
+import IndexScreen from "./screens/IndexScreen";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        mode="model"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#008b8b",
+          },
+          headerTintColor: "#ffff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title:'MainPage' }}
+        />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
